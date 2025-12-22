d4201ef770137073847a373709deac1c061210e6
Asela Sandaruwan <108016651+Asela2001@users.noreply.github.com>
2025-12-22 23:59:55 +0530

    last

--- src/users/dto/change-password.dto.ts
+++ src/users/dto/change-password.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MinLength, IsNotEmpty, Matches } from 'class-validator';
+import { IsString, MinLength, IsNotEmpty } from 'class-validator';
 
 export class ChangePasswordDto {
   @IsString()
@@ -12,12 +12,12 @@ export class ChangePasswordDto {
 
   @IsString()
   @IsNotEmpty({ message: 'Confirm new password required' })
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
-    message: 'Passwords must match',
-  }) // Match newPassword (custom validator if needed)
+  // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
+  //   message: 'Passwords must match',
+  // }) // Match newPassword (custom validator if needed)
   confirmNewPassword: string;
 }
-function createValidator(arg0: (value: any, args: any) => { matchNewPassword: boolean; } | null) {
-    throw new Error('Function not implemented.');
-}
+// function createValidator(arg0: (value: any, args: any) => { matchNewPassword: boolean; } | null) {
+//     throw new Error('Function not implemented.');
+// }
 
